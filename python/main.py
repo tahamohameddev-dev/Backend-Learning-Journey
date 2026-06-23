f983a5a1b4c6492a631f97c32b59770205431651
Taha Mohamed <tahamohamed.dev@gmail.com>
2026-06-23 15:25:36 +0300

    added progtame-test
    and new Edit for main.py

--- python/main.py
+++ python/main.py
@@ -17,6 +17,71 @@ is_Developer = True
 print(name)
 
 # ####################################
-# 03. CONDITIONS
+# 03. WORKING WITH STRINGS & NUMBERS
 # ####################################
+
+phrase = "Backend Academy"
+
+print(phrase.upper())
+print(phrase.lower())
+
+print(phrase.isupper())
+print(phrase.islower())
+
+print(len(phrase))
+
+print(phrase.index("B"))
+print(phrase.replace("Academy","Lerning"))
+
+# NUMBERS
+
+my_num = 5
+print(str(my_num) + " my favorite number")
+
+print(3 + 4)
+print(3 - 4)
+print(3 / 4)
+print(3 * 4)
+
+print(abs(-5))
+print(pow(4, 2))
+print(max(9, 8))
+print(min(4, 7))
+print(round(7.5))
+
+# ####################################
+# 0.4 GITTING INPUT & BASIC CALCULATOR
+# ####################################
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Hello " + name + " your age " + age + "!")
+
+
+# Calculator
+
+num_1 = input("Enter number: ")
+num_2 = input("Enter number: ")
+
+Calculation = int(num_1) + int(num_2)
+print(Calculation)
+
+# ####################################
+# 0.5 LISTS & LIST FUNCTIONS & TUPELS 
+# ####################################
+
+skills = ["Python", "Linux", "Git"]
+skills.append("Databases")
+skills.insert(1,"Docker")
+skills.remove("Git")
+# skills.clear()
+print(skills )
+print(skills.index("Python"))
+print(skills.count("Linux"))
+
+skills2 = skills.copy()
+
+print(skills2)
+
+# Tuples
 
--- test-Python.py
+++ test-Python.py
@@ -1,2 +1,24 @@
-print("hello world")
-print("hello")
+
+name = input("Enter your name: ")
+print("your name is ", name.upper())
+print(name.title())
+print(len(name))
+
+
+
+package_price = input("Enter package price: ")
+Discount = input("Enter Discount: ")
+
+Calculation = int(package_price) - int(Discount)
+
+print(f"prise after descount ", {Calculation})
+
+print( "your num: " , {min(50, Calculation)})
+
+
+vip_users = ["Ahmed", "Saeed"]
+new_name = input("Enter your name: ")
+vip_users.append(new_name)
+
+vip_users.insert(1, "Hacker")
+print(vip_users)
